47b5f678c154840bebc34ccd101490bbd6b1c774
mr-meetpatel <pm2410.ldrp@gmail.com>
2023-02-28 15:31:45 +0530

    Schedule Result on every friday

--- main.py
+++ main.py
@@ -30,26 +30,34 @@ def send_message(channel_id, text):
     client.chat_postMessage(channel=channel_id, text=text)
 
 def calculate_wordle_stats():
+    global stats_count
     stats = """
     <!channel>
     Stats for Attempted WORDLE for the week of {} to {}
-    There are {} royalties this week.. :crown:
+    There {} this week.. :crown:
     Check your stats below :tada:
-    *Royalties List* :trophy:
-    {}
-    *Remaining Players List* :clap:
+    
     {}
     """
-    no_of_royalties = sum(stats_count[user][0] == 1 for user in stats_count)
+    no_of_royalties = sum(stats_count[user][0] == 5 for user in stats_count)
+    msg = f"is {no_of_royalties} royalty"
+    if no_of_royalties > 1:
+         msg = f"are {no_of_royalties} royalties"
+
     royalties = ""
     non_royalties = ""
     for user in stats_count:
-        if stats_count[user][0]==1:
-            royalties+=f"<@{user}>\n"
-        else:
-            non_royalties+=f"<@{user}>\n"
-    send_message("#test",stats.format((date.today() - timedelta(4)).strftime("%d %b"),date.today().strftime("%d %b"),no_of_royalties,royalties,non_royalties))
+        if stats_count[user][0]==5:
+            if not royalties:
+                royalties += f"*Leaderboard* :trophy:\n"
 
+            royalties+=f"- <@{user}>\n"
+        else:
+            if not non_royalties:
+                non_royalties += "*\tRemaining Players List* :clap:\n"
+            non_royalties+=f"- {client.users_info(user=user)['user']['real_name']} ({stats_count[user][0]}/5)\n"
+    stats_count = {}
+    send_message("#test",stats.format((date.today() - timedelta(4)).strftime("%d %b"),date.today().strftime("%d %b"),msg,royalties+non_royalties))
 @slack_events_adapter.on("message")
 def message(payload):
     response = payload.get("event", {})
@@ -60,7 +68,7 @@ def message(payload):
         reaction = "tada"
         today = datetime.now().date().strftime("%Y-%m-%d")
         if user_id in stats_count:
-            if str(stats_count[user_id][1]) != today:
+            if str(stats_count[user_id][1]) != today and stats_count[user_id][0] <= 4:
                 stats_count[user_id][0] += 1
                 send_message(user_id, f"{GREETING_MESSAGE.format(user_id)}")
         else:
@@ -70,15 +78,16 @@ def message(payload):
         # Adding Reaction base on score
         if text[11] == "X":
             reaction="thumbsup"
-        elif text[11] == "1":
+        elif text[11] in ["1","2"]:
             reaction = "fire"
+        print(user_id)
         client.reactions_add(name=reaction,channel=channel_id,timestamp=response["ts"])
-
 @app.route("/")
 def index():
     schedule.every().day.at("06:00").do(send_message, "#test", "Good Morning!")
-    schedule.every().tuesday.at("07:00").do(send_message,"#test","Happy Tuesday ...")
-    schedule.every().day.at("21:36").do(calculate_wordle_stats)
+    schedule.every().wednesday.at("07:00").do(send_message,"#test","Happy Wednesday ...")
+    schedule.every().day.at("15:29").do(send_message,"U04RLBVPJ48","Bot is Working...")
+    schedule.every().friday.at("18:30").do(calculate_wordle_stats)
     while True:
         schedule.run_pending()
         time.sleep(1)
