52bc8746c00bf9813cdf9284c01421c955f5850d
mr-meetpatel <pm2410.ldrp@gmail.com>
2023-02-28 15:30:20 +0530

    Update Regex pattern

--- constants.py
+++ constants.py
@@ -1,2 +1,2 @@
-WORDLE_REGEX="^(Wordle)\s\d{3,}\s(\d|X)\/\d(\\n){2}:(white_large_square(::|:|:\\n:)|large_yellow_square(::|:|:\\n:)|large_green_square(::|:|:\\n:)){30}$"
+WORDLE_REGEX="^(Wordle)\s\d{3,}\s(\d|X)\/\d(\\n){2}:(white_large_square(::|:|:\\n:)|large_yellow_square(::|:|:\\n:)|large_green_square(::|:|:\\n:)){5,30}$"
 GREETING_MESSAGE = "Hii <@{}>, Thank you solving today's wordle :tada:"
