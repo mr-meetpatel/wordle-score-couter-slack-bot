a252c082d3aba53b2d0507f13592f18f5a60dafa
Meet Patel <pm2410.ldrp@gmail.com>
2024-08-31 20:05:52 +0530

    chore: update constants

--- constants.py
+++ constants.py
@@ -1,2 +1,16 @@
-WORDLE_REGEX="^(Wordle)\s\d{3,}\s(\d|X)\/\d(\\n){2}:(white_large_square(::|:|:\\n:)|large_yellow_square(::|:|:\\n:)|large_green_square(::|:|:\\n:)){5,30}$"
+WORDLE_REGEX="^(Wordle)\s[\d,]+\s(?P<score>(\d|X))\/\d[*]?(\\n){2}:(white_large_square(::|:|:\\n:)|large_yellow_square(::|:|:\\n:)|large_green_square(::|:|:\\n:)|black_large_square(::|:|:\\n:)|large_orange_square(::|:|:\\n:)|large_blue_square(::|:|:\\n:)){5,30}$"
 GREETING_MESSAGE = "Hii <@{}>, Thank you solving today's wordle :tada:"
+NO_OF_DAYS = 5
+
+"""
+Workspace Config
+"""
+# My Workspace
+SIGING_SECRET = ""
+SLACK_OAUTH_TOKEN = ""
+CHANNEL_NAME = "#test"
+
+# ScaleReal Workspace
+# SIGING_SECRET = ""
+# SLACK_OAUTH_TOKEN = ""
+# CHANNEL_NAME = "#channel-english-101"
